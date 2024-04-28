f55a7d31286559df59d613010e8a5b9a755a6f16
Akeu-Andrade <akeusantana@gmail.com>
2024-04-28 02:41:01 -0300

    ✅ adicioa novo endpoint que adiciona ao carrinho

--- src/application/controllers/CartController.ts
+++ src/application/controllers/CartController.ts
@@ -1,15 +1,19 @@
 import { inject, injectable } from "tsyringe";
 import { NextFunction, Request, Response } from "express";
 import { ICreateCartUseCase } from "../../domain/usecases/cart/ICreateCartUseCase";
+import { AddProductToCartUseCase } from "../useCases/cart/AddProductToCartUseCase";
 
 @injectable()
 export class CartController {
     private createCartUseCase: ICreateCartUseCase;
+    private addProductToCartUseCase: AddProductToCartUseCase;
 
     constructor(
         @inject("ICreateCartUseCase") createCartUseCase: ICreateCartUseCase,
+        @inject("IAddProductToCartUseCase") addProductToCartUseCase: AddProductToCartUseCase
     ) {
         this.createCartUseCase = createCartUseCase;
+        this.addProductToCartUseCase = addProductToCartUseCase;
     }
 
     createCart = async (request: Request, response: Response, next: NextFunction): Promise<void> => {
@@ -17,6 +21,17 @@ export class CartController {
             const { userId } = request.body;
             const cart = await this.createCartUseCase.invoke(userId ? userId : null);
             response.status(201).json(cart);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    addProductToCart = async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+        try {
+            const { productId, quantity } = request.body;
+            const { cartId } = request.params;
+            const cart = await this.addProductToCartUseCase.invoke(cartId, productId, quantity);
+            response.status(200).json(cart);
         } catch (error) {
             next(error);
         }
